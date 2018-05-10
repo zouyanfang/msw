ccc7e4ac80c91fa40b45036cb05c890ec87ecd86
zouyanfang <2945176607@qq.com>
2018-05-10 17:15:08 +0800

    稍作修改

--- controllers/index.go
+++ controllers/index.go
@@ -14,14 +14,14 @@ type IndexController struct {
 func (this *IndexController)Index(){
 	//首页轮播图展示
 	page := 1
-	slideShow := services.GetDishList(page,utils.PAGESIZE3,"AND release_role = 2 AND is_slideshow = 0 ORDER BY release_date DESC",nil)
+	slideShow := services.GetDishList(page,utils.PAGESIZE3,"AND release_role = 2 AND is_slideshow = 1 ORDER BY release_date DESC",nil)
 	this.Data["dish"] = slideShow.Object
 
 	//首页菜单列表展示
 	menuShow := services.GetMenuList(page,utils.PAGESIZE4,"ORDER BY popular_count DESC",nil)
 	this.Data["menu"] = menuShow.Object
 	//首页菜谱列表展示
-	dishShow := services.GetDishList(page,utils.PAGESIZE6,"ORDER BY release_date DESC",nil)
+	dishShow := services.GetDishList(page,utils.PAGESIZE6,"AND is_slideshow = 2 ORDER BY release_date DESC",nil)
 	this.Data["dishShow"] = dishShow.Object
 	//TODO
 	//首页最新消息展示
